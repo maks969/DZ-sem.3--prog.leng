3b8c1e42835307b567d27fa1c35105aa22c755c0
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-20 23:18:15 +0300

    new

---
+++ task 23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+
+void PowX3(double A, out double B)
+{            
+    B = Math.Pow(A,3);            
+}
+//double A = 10; 
+double C= 0;
+Console.WriteLine("введите число");
+double i = double.Parse(Console.ReadLine()!);
+int A = 0;
+for (A=0;i>A;A++)
+{
+PowX3(A, out C);
+ Console.WriteLine(C);
+}
