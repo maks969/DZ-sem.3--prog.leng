4d10fd9fe6ff8d7a1d30fd2011843f692d293353
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-23 22:22:45 +0300

    end 1

---
+++ task 21/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек
+ и находит расстояние между ними в 3D пространстве.*/
+ 
+
+Console.WriteLine("введите координаты первой точки");
+Console.WriteLine("введите X");
+double x=double.Parse(Console.ReadLine());
+Console.WriteLine("введите Y");
+double y=double.Parse(Console.ReadLine());
+Console.WriteLine("введите Z");
+double z=double.Parse(Console.ReadLine());
+
+Console.WriteLine("введите координаты второй точки");
+Console.WriteLine("введите X1");
+double x1=double.Parse(Console.ReadLine());
+Console.WriteLine("введите Y1");
+double y1=double.Parse(Console.ReadLine());
+Console.WriteLine("введите Z1");
+double z1=double.Parse(Console.ReadLine());
+
+double h = Math.Sqrt(Math.Pow(x1-x,2)+Math.Pow(y1-y,2)+Math.Pow(z1-z,2));
+
+
+Console.WriteLine($"расстоянте = {h}");
+
