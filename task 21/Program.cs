17d7264ac11c03556bf3d08aef15f3fda3610db8
maks969 <111703818+maks969@users.noreply.github.com>
2022-09-29 15:54:51 +0300

    we have done with the solution

--- task 21/Program.cs
+++ task 21/Program.cs
@@ -4,19 +4,19 @@
 
 Console.WriteLine("введите координаты первой точки");
 Console.WriteLine("введите X");
-double x=double.Parse(Console.ReadLine());
+double x=double.Parse(Console.ReadLine()!);
 Console.WriteLine("введите Y");
-double y=double.Parse(Console.ReadLine());
+double y=double.Parse(Console.ReadLine()!);
 Console.WriteLine("введите Z");
-double z=double.Parse(Console.ReadLine());
+double z=double.Parse(Console.ReadLine()!);
 
 Console.WriteLine("введите координаты второй точки");
 Console.WriteLine("введите X1");
-double x1=double.Parse(Console.ReadLine());
+double x1=double.Parse(Console.ReadLine()!);
 Console.WriteLine("введите Y1");
-double y1=double.Parse(Console.ReadLine());
+double y1=double.Parse(Console.ReadLine()!);
 Console.WriteLine("введите Z1");
-double z1=double.Parse(Console.ReadLine());
+double z1=double.Parse(Console.ReadLine()!);
 
 double length = Math.Sqrt(Math.Pow(x1-x,2)+Math.Pow(y1-y,2)+Math.Pow(z1-z,2));
 
